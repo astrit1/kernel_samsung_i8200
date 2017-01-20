6191fafcea4bf550ecda7d4c437e985546cef1a3
astrit <a.haxhiu@web.de>
2017-01-21 00:03:31 +0100

    Disable CRC for more Performance

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -59,7 +59,7 @@ struct device *sec_sdcard_dev_t;
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 1;
+bool use_spi_crc = 0;
 module_param(use_spi_crc, bool, 0);
 
 /*
